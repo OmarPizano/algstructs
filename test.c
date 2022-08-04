d51b034c81efd3373e0a4ff6f1184ce7e4dc45f5
OmarPizano <oj.pizanoorozco@ugto.mx>
2022-08-04 16:10:26 -0500

    Use function parameter to print (or not) the sorted array. Free allocated memory.

--- test.c
+++ test.c
@@ -2,40 +2,43 @@
 #include "utils.h"
 #include <stdio.h>
 #include <time.h>
+#include <stdbool.h>
 
-void test_bubble(int *arr, unsigned int len)
+void test_bubble(int *arr, unsigned int len, bool print)
 {
-        print_array(arr, len);
+        if (print) print_array(arr, len);
         int time1 = time(NULL);
         sort_bubble(arr, len);
         int time2 = time(NULL);
-        print_array(arr, len);
-        printf("BUBBLE EXEC TIME: %d seconds.\n", time2-time1);
+        if (print) print_array(arr, len);
+        printf("BUBBLE EXEC TIME: %d seconds.\n\n", time2-time1);
 
 }
 
-void test_insertion(int *arr, unsigned int len)
+void test_insertion(int *arr, unsigned int len, bool print)
 {
-        print_array(arr, len);
+        if (print) print_array(arr, len);
         int time1 = time(NULL);
         sort_insertion(arr, len);
         int time2 = time(NULL);
-        print_array(arr, len);
-        printf("INSERTION EXEC TIME: %d seconds.\n", time2-time1);
+        if (print) print_array(arr, len);
+        printf("INSERTION EXEC TIME: %d seconds.\n\n", time2-time1);
 
 }
 
 int main(void)
 {
         int *arr1;
-        
-        arr1 = gen_randarray(15, 999, 1337);
-        test_bubble(arr1, 15);
-        
-        arr1 = gen_randarray(15, 999, 1337);
-        test_insertion(arr1, 15);
+        unsigned const int LEN = 15;
+
+        arr1 = gen_randarray(LEN, 999, 1337);
+        test_bubble(arr1, LEN, true);
+        free(arr1);
+
+        arr1 = gen_randarray(LEN, 999, 1337);
+        test_insertion(arr1, LEN, true);
+        free(arr1);
        
-        // TODO: free array
         return 0;
 }
 
