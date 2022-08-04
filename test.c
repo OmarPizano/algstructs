7627c1c5ea91ed28bc028e63c2ac603014e4df1f
OmarPizano <oj.pizanoorozco@ugto.mx>
2022-08-04 09:26:39 -0500

    Update bubble test function to recieve the size of the test array.

--- test.c
+++ test.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 #include <time.h>
 
-void test_bubble(int *arr)
+void test_bubble(int *arr, unsigned int len)
 {
         int time1 = time(NULL);
         
-        sort_bubble(arr, 15);
+        sort_bubble(arr, len);
         
         int time2 = time(NULL);
         printf("BUBBLE EXEC TIME: %d seconds.\n", time2-time1);
@@ -18,7 +18,7 @@ int main(void)
         
         int arr1[15] = {9,43,18,200,38,42,1337,28,98,17,438,2989,28,9,18};
 
-        test_bubble(arr1);
+        test_bubble(arr1, 15);
         
         return 0;
 }
